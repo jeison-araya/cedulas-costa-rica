b5d1f9b6e4218e49fd0d79756470437ab8364827
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2022-07-14 16:48:39 -0600

    Cargar padron desde csv

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import csv
 from fastapi import FastAPI, HTTPException, status
 from api.database import database
 
@@ -11,10 +12,35 @@ exceptions = {
 
 @app.get("/padron/cedula/{cedula:str}")
 def obtener_por_cedula(cedula: str):
-    
-    persona = database["padron"].find_one({"cedula": cedula})
+    persona = database["padron"].find_one({"cedula": cedula}, {"_id": False})
     
     if not persona:
         raise exceptions["PersonaNoEncontrada"]
     
     return persona
+
+@app.post("/padron")
+def cargar_padron():
+    file_path = "tmp/PADRON_COMPLETO.txt"
+    
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        line_count = 0
+        headers = [
+                "cedula", "codigo_electoral", "relleno",
+                "fecha_caducidad", "junta", "nombre",
+                "primer_apellido", "segundo_apellido"
+                ]
+        for row in csv_reader:
+            person = {}
+            
+            for header, value in zip(headers, row):
+                if header != "relleno":
+                    person[header] = value.strip()
+
+            database["padron"].insert_one(person)
+            line_count += 1
+        print(f"Rows readed: {line_count}")
+        rows_inserted = database["padron"].count_documents({})
+        print(f"Rows inserted: {rows_inserted}")
+    
