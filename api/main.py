80c5e919a51d3a7acd3744a14218c8b321d9e231
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2022-07-14 20:01:02 -0600

    Modelo persona agregado

--- api/main.py
+++ api/main.py
@@ -1,19 +1,55 @@
+"""
+API padrón electoral Costa Rica.
+"""
 from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 from api.database import database
 
 app = FastAPI()
 
 exceptions = {
     "PersonaNoEncontrada": HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                         detail="E001: Persona no encontrada")
+                                         detail="E001: Persona no encontrada"),
+    "FormatoCedulaNoValida": HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                         detail="E002: Formato de cédula no válido"),
 }
 
 
-@app.get("/padron/cedula/{cedula:str}")
-def obtener_por_cedula(cedula: str):
-    persona = database["padron"].find_one({"cedula": cedula}, {"_id": False})
-    
+class Persona(BaseModel):
+    """
+    Persona
+
+    Representa una persona que pertence al padrón electoral.
+
+    Atributos:
+
+    - cedula: Número de cédula del ciudadano sin guiones,
+              sin puntos y longitud igual a 9.
+    - nombre: Nombre completo del ciudadano
+    - primer_apellido: Primer apellido
+    - segundo_apellido Segundo apellido
+    """
+    cedula: str
+    nombre: str
+    primer_apellido: str
+    segundo_apellido: str
+
+
+padron = database["padron_1"]
+
+@app.get("/padron/cedula/{cedula:int}", response_model=Persona)
+def obtener_por_cedula(cedula: int):
+    """
+    GET - Obtener persona por número de cédula.
+
+    Formato de la cédula: numero de cédula sin guiones, sin puntos y longitud igual a 9.
+    """
+    if cedula < 1000000000 or cedula > 9999999999:
+        raise exceptions["FormatoCedulaNoValida"]
+
+    persona = padron.find_one({"cedula": cedula})
+
     if not persona:
         raise exceptions["PersonaNoEncontrada"]
-    
+
     return persona
